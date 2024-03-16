c5d9440a86517f30da85e130320eaa36e91b9653
vinicius <vinicius.souza.assis@gmail.com>
2024-03-16 12:58:26 -0300

    feat: api

--- src/server.js
+++ src/server.js
@@ -25,8 +25,8 @@ app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(cookieParser());
 app.use(
 	cors({
-		/* origin:
-			"rocket-food-explorer.netlify.app" */ /* ["http://localhost:5173", "http://127.0.0.1:5173"],*/
+		origin:
+			"rocket-food-explorer.netlify.app" /* ["http://localhost:5173", "http://127.0.0.1:5173"] */,
 		credentials: true,
 	})
 );
